f82a8951b9749fed529bf3e45e67e4b1911006d9
Jacob Hummer <jcbhmr@outlook.com>
2023-08-15 22:10:37 -0500

    Create lib.sh

---
+++ lib.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+apt_get_update()
+{
+    if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+        echo "Running apt-get update..."
+        apt-get update -y
+    fi
+}
+
+# Checks if packages are installed and installs them if not
+check_packages() {
+    if ! dpkg -s "$@" > /dev/null 2>&1; then
+        apt_get_update
+        apt-get -y install --no-install-recommends "$@"
+    fi
+}
