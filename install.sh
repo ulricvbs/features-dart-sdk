38d7626336f6e955920074a70485d1f00e6f5e01
Jacob Hummer <jcbhmr@outlook.com>
2023-08-15 22:09:45 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,39 +1,10 @@
-#!/bin/sh
-set -eux
+#!/bin/bash
+set -ex
+source lib.sh
 
-BASEURL="https://storage.googleapis.com/dart-archive/channels"
-DART_VER=$VERSION
-export DEBIAN_FRONTEND="noninteractive"
-apt update
-apt install -y --no-install-recommends \
-    curl \
-    ca-certificates \
-    unzip \
-    xz-utils \
-    zip \
-    file
+check_packages wget gpg
 
-case "$(dpkg --print-architecture)" in
-amd64)
-    DART_SHA256=cccd5300faa5a9abce12a5f77586e26350028cea82bb4ff8eeb55641b58a2e1d
-    SDK_ARCH="x64"
-    ;;
-armhf)
-    DART_SHA256=3a15d42cb1677ac5e50a23045cafe3bf5db2855a5287a3e9019b849fe8477897
-    SDK_ARCH="arm"
-    ;;
-arm64)
-    DART_SHA256=2c8eeaf0d3da60c4e14beec45ce3b39aca754f71b9fa3fb0c635ee28d6f44708
-    SDK_ARCH="arm64"
-    ;;
-esac
-
-SDK="dartsdk-linux-${SDK_ARCH}-release.zip"
-URL="$BASEURL/stable/release/${DART_VER}/sdk/$SDK"
-curl -fLO "$URL"
-echo "$DART_SHA256 *$SDK" | sha256sum --check --status --strict -
-unzip "$SDK"
-mv dart-sdk "$DART_ROOT"
-rm "$SDK"
-chmod 755 "$DART_ROOT"
-chmod 755 "$DART_ROOT/bin"
+sudo apt-get update
+sudo apt-get install apt-transport-https
+wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo gpg --dearmor -o /usr/share/keyrings/dart.gpg
+echo 'deb [signed-by=/usr/share/keyrings/dart.gpg arch=amd64] https://storage.googleapis.com/download.dartlang.org/linux/debian stable main' | sudo tee /etc/apt/sources.list.d/dart_stable.list
