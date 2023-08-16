89e99703586045494fa386031dfd719fe1bc4c50
Jacob Hummer <jcbhmr@outlook.com>
2023-08-15 22:16:22 -0500

    Update lib.sh

--- lib.sh
+++ lib.sh
@@ -15,3 +15,15 @@ check_packages() {
         apt-get -y install --no-install-recommends "$@"
     fi
 }
+
+updaterc() {
+    # if [ "${UPDATE_RC}" = "true" ]; then
+        echo "Updating /etc/bash.bashrc and /etc/zsh/zshrc..."
+        if [[ "$(cat /etc/bash.bashrc)" != *"$1"* ]]; then
+            echo -e "$1" >> /etc/bash.bashrc
+        fi
+        if [ -f "/etc/zsh/zshrc" ] && [[ "$(cat /etc/zsh/zshrc)" != *"$1"* ]]; then
+            echo -e "$1" >> /etc/zsh/zshrc
+        fi
+    # fi
+}
