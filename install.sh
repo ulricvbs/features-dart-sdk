97e9bb2d65b40dc0c50ec81fc24bfacabc03fdec
Jacob Hummer <jcbhmr@outlook.com>
2023-08-15 22:22:52 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,13 @@ wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | gpg --dearmor 
 echo 'deb [signed-by=/usr/share/keyrings/dart.gpg arch=amd64] https://storage.googleapis.com/download.dartlang.org/linux/debian stable main' >/etc/apt/sources.list.d/dart_stable.list
 
 apt-get update
-apt-get install dart
+if [[ $VERSION == latest ]]; then
+  apt-get install dart
+else
+  apt-get install dart=$VERSION
+fi
 
 updaterc 'export PATH="$PATH:/usr/lib/dart/bin"'
+
+# Clean up
+rm -rf /var/lib/apt/lists/*
